507d680638edc2e6eaf19274c413452aa04e2cac
Lavelle Blackwater <you@example.com>
2023-01-09 02:55:34 -0700

    EOD status update on Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -32,8 +32,16 @@ fifth_pick = random.choice(colors)
 sixth_pick = random.choice(apps)
 def hangman():
     if og_input == 'countries':
+        new_countries = []
         countries_choice  = first_pick
         return countries_choice
+        
+        countries_word = list(countries_choice)
+        # countries_letter = str(input('Please enter a letter: '))
+        # if countries_letter in countries_word:
+        #         print('correct!')
+
+
     elif og_input == 'flowers':
         flowers_choice = second_pick
         return flowers_choice
@@ -48,7 +56,8 @@ def hangman():
         return colors_choice
     elif og_input == 'apps':
         apps_choice = sixth_pick
-        return apps_choice
+        return apps_choice 
+
     else:
         print('Please select from the choices listed!')
 hangman()
@@ -60,7 +69,9 @@ hangman()
 #work on - print one category, correct one, 
 
 #1/9 - 02:21 - set up to print category
-#work on - 
+#work on - guess letter, in if loop? if letter matches with one in chosen word, add to a new empty list
+#list - need a way to match letters with word returned, 
+#need to make word returned into new list to match letters to new word
 
 	
 
